4f3c6bdddca561e369f3cabd65095b0c85744c5f
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-02-28 22:06:16 -0600

    agregado el cambio de sprites con la rueda del raton, se guarda y carga la escena, seevita poner sprites sobre los botones dejando bloqueado un sector de la pantalla, casi todas las funciones del prototipo funcionan, se necesita implementar la seleccion de archivos y cargar el mapa a editar

--- scripts/editorScript.gd
+++ scripts/editorScript.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 #primero funcion los botones izquierdo y derecho
-#la escena debe tener mas controles, cambiando los tiles a colocar
 #codigo de prototipo finalizado 21/sep/19
+#ultimo avance 28/feb/20
 # :3
 #de anubarak para quien lo necesite
 #licencia MIT :D
 
 var loDeLosTiles
+var spriteActual = 0
 
 func _ready():
 	loDeLosTiles = get_node("loDeLosTiles")
@@ -15,9 +16,13 @@ func _ready():
 
 func _input(event):
 	if(event is InputEventMouseButton and event.is_pressed()):
+		if(event.button_index == BUTTON_WHEEL_UP or event.button_index == BUTTON_WHEEL_DOWN):
+			if(spriteActual == 0):
+				spriteActual = 1
+			else:
+				spriteActual = 0
 		#toma las presiones del mouse despues de el punto 130
 		#esto por que no quiero estorbarle a los botones
-		
 		#¿ahora como carajos muevo la camara con la posicion del mouse
 		#sin romper ese limite arbitrario?
 		if(event.position.x > 130):
@@ -25,8 +30,8 @@ func _input(event):
 				#en caso de presionar el boton izquierdo
 				var tilePos = loDeLosTiles.world_to_map(event.position)
 				#toma un vector(x,y) y lo traduce en coordenadas de malla
-				loDeLosTiles.set_cellv(tilePos, 0)
-				#cambia la celda de la posicion por el sprite 0
+				loDeLosTiles.set_cellv(tilePos, spriteActual)
+				#cambia la celda de la posicion por el sprite(0:yerba 1:tierra)
 			if(event.button_index == BUTTON_RIGHT and event.pressed):
 				#igual pero con el boton contrario
 				var tilePos = loDeLosTiles.world_to_map(event.position)
@@ -48,3 +53,8 @@ func cargar():
 	print(loDeLosTiles.get_name())
 	desarchivar.close()
 	add_child(loDeLosTiles)
+	
+func salir():
+	guardar()
+	
+	pass
