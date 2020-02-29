2a466567ee9e1027b5d3f9494d91cd023ca73ef1
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-02-28 18:13:31 -0600

    de alguna manera, se puede presionar los botones sin manchar el TileMap de abajo

--- scripts/editorScript.gd
+++ scripts/editorScript.gd
@@ -14,21 +14,25 @@ func _ready():
 	#se toma un puntero al nodo hijo para mayor comodidad
 
 func _input(event):
-	if(event is InputEventMouseButton):
-		#toma las presiones del mouse
-		if(event.button_index == BUTTON_LEFT and event.pressed):
-			#en caso de presionar el boton izquierdo
-			var tilePos = loDeLosTiles.world_to_map(event.position)
-			#toma un vector(x,y) y lo traduce en coordenadas de malla
-			loDeLosTiles.set_cellv(tilePos, 0)
-			#cambia la celda de la posicion por el sprite 0
+	if(event is InputEventMouseButton and event.is_pressed()):
+		#toma las presiones del mouse despues de el punto 130
+		#esto por que no quiero estorbarle a los botones
 		
-		if(event.button_index == BUTTON_RIGHT and event.pressed):
-			#igual pero con el boton contrario
-			var tilePos = loDeLosTiles.world_to_map(event.position)
-			#exactamente lo mismo, pero la posicion cambiará
-			loDeLosTiles.set_cellv(tilePos, -1)
-			#un -1 va a eliminar el sprite en ese sitio de la malla
+		#¿ahora como carajos muevo la camara con la posicion del mouse
+		#sin romper ese limite arbitrario?
+		if(event.position.x > 130):
+			if(event.button_index == BUTTON_LEFT and event.pressed):
+				#en caso de presionar el boton izquierdo
+				var tilePos = loDeLosTiles.world_to_map(event.position)
+				#toma un vector(x,y) y lo traduce en coordenadas de malla
+				loDeLosTiles.set_cellv(tilePos, 0)
+				#cambia la celda de la posicion por el sprite 0
+			if(event.button_index == BUTTON_RIGHT and event.pressed):
+				#igual pero con el boton contrario
+				var tilePos = loDeLosTiles.world_to_map(event.position)
+				#exactamente lo mismo, pero la posicion cambiará
+				loDeLosTiles.set_cellv(tilePos, -1)
+				#un -1 va a eliminar el sprite en ese sitio de la malla
 
 func guardar():
 	var archivador = File.new()
