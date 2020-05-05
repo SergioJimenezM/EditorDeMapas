7275c2d8228d759477ba961e983ec6282f62afea
SergioJimenezM <anubarakdewaarimasen@gmail.com>
2020-05-04 20:51:51 -0600

    reparada la camara

--- scripts/camara.gd
+++ scripts/camara.gd
@@ -3,6 +3,8 @@ extends Camera2D
 export var velocidadDeLaCamara = 15
 
 func _process(delta):
+	var posicion =  get_global_position()
+	var botonera = $botonera.get_global_position()
 	#Guarda un bit hacia la direccion del movimiento en eje X
 	var moverX = int(Input.is_action_pressed("ui_left")) - int(Input.is_action_pressed("ui_right"))
 	#guarda un bit hacia la direccion del movimiento en eje Y
@@ -10,17 +12,23 @@ func _process(delta):
 	#Se necesita el delta * velocidad de movimiento dos veces en la formula
 	#por tanto se aplica esta parte de la formula ahorrando un proceso
 	var DeltaMultiplicado = velocidadDeLaCamara * delta
-	position.x = lerp(position.x, position.x - moverX * velocidadDeLaCamara, DeltaMultiplicado)
-	position.y = lerp(position.y, position.y - moverY * velocidadDeLaCamara, DeltaMultiplicado)
+	posicion.x = lerp(posicion.x, posicion.x - moverX * velocidadDeLaCamara, DeltaMultiplicado)
+	posicion.y = lerp(posicion.y, posicion.y - moverY * velocidadDeLaCamara, DeltaMultiplicado)
 	#lerp es interpolacion lineal
 	#consigue un valor perteneciente a la función asociada que estamos creando
 	#la formula traza una recta entre dos valores y así se calcula el movimiento
-#	if position.x <= 1000 and position.x >= -1000:
-#		position.x = lerp(position.x, position.x - moverX * velocidadDeLaCamara, DeltaMultiplicado)
-#	else:
-#		position.x = position.x + 10 + (-1 * moverX) #rebote por limite del mapa
+	print($botonera.get_global_position())
+	#tomar la botonera y fijarla en posiciones relativas a estas
+	if posicion.x >= 680:#No son 1200 reales
+		posicion.x = posicion.x - 10
+	if posicion.x <= -685:
+		posicion.x = posicion.x + 10
+	if posicion.y >= 900:
+		posicion.y = posicion.y - 10
+	if posicion.y <= -900:
+		posicion.y = posicion.y + 10
+	botonera.x = (posicion.x - 525)
+	botonera.y = (posicion.y - 290)
+	$botonera.set_global_position(botonera)#hay que alterar esta posicion
+	set_global_position(posicion)
 	
-#	if position.y <= 1000 and position.y >= -1000:
-#		position.y = lerp(position.y, position.y - moverY * velocidadDeLaCamara, DeltaMultiplicado)
-#	else:
-#		position.y = position.y + 10 +(-1 * moverY) #rebote por limite del mapa y invertir signo
